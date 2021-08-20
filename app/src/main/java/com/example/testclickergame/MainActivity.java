71b4757335fcdb552ad515366e387fe918f7cf25
Максим Толстобров <maximah1989@yandex.ru>
2021-08-20 14:35:20 +0300

    Create many things

--- app/src/main/java/com/example/testclickergame/MainActivity.java
+++ app/src/main/java/com/example/testclickergame/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.testclickergame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 
 import com.example.testclickergame.databinding.ActivityMainBinding;
@@ -22,22 +24,21 @@ import java.util.concurrent.TimeUnit;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Completable;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Observer;
-import io.reactivex.rxjava3.core.Single;
-import io.reactivex.rxjava3.core.SingleObserver;
-import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.observers.DisposableCompletableObserver;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 @SuppressLint("SetTextI18n")
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "TAG";
 
     private ActivityMainBinding binding;
 
-    private AnimationDrawable mAnimation;
+    private AnimationDrawable mAnimationPlayer;
+    private AnimationDrawable mAnimationEnemy;
     private long totalDuration = 0;
     private boolean inGame = false;
+    private int totalDamage = 0;
+    private int hpEnemy = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,26 +46,51 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this));
         setContentView(binding.getRoot());
         initView();
-        getListBitmapPlayer();
 
-        fillAnimationPlayer(mAnimation, getListBitmapPlayer());
-        binding.imageView.setBackground(mAnimation);
+        fillAnimation(mAnimationPlayer, getListBitmapOrcSlashing(), 40);
+        fillAnimation(mAnimationEnemy, getListBitmapGoblinHurt(), 40);
 
-        binding.constrainRoot.setOnClickListener(v -> startGame());
+        binding.imageViewPlayer.setBackground(mAnimationPlayer);
+        binding.imageViewEnemy.setBackground(mAnimationEnemy);
+
         binding.stopGame.setOnClickListener(v -> stopGame());
+        binding.constrainRoot.setOnClickListener(v -> {
+            stopAnimation(mAnimationPlayer);
+            startAnimation(mAnimationPlayer);
 
+            stopAnimation(mAnimationEnemy);
+            startAnimation(mAnimationEnemy);
+
+            showScore();
+            updateHpEnemy();
+        });
     }
 
-    private void startGame() {
-        inGame = true;
-        stopAnimation(mAnimation);
-        updateData(getTotalDuration(mAnimation));
-        startAnimation(mAnimation);
+    private void updateHpEnemy() {
+        totalDamage += 5;
+        int currentEnemyHp = hpEnemy - totalDamage;
+        binding.pbHorizontal.setProgress(currentEnemyHp);
+        if (currentEnemyHp < 0) {
+            binding.textHpEnemy.setText("0");
+            generateEnemy();
+        } else {
+            binding.textHpEnemy.setText(String.valueOf(currentEnemyHp));
+        }
     }
+
+    private void generateEnemy() {
+        hpEnemy = Utils.getHpEnemy(Utils.currentLevelScene);
+        binding.pbHorizontal.setMax(hpEnemy);
+        binding.pbHorizontal.setProgress(hpEnemy);
+        binding.textHpEnemy.setText(String.valueOf(hpEnemy));
+        totalDamage = 0;
+        updateScore(10);
+    }
+
 
     private void stopGame() {
         inGame = false;
-        stopAnimation(mAnimation);
+        stopAnimation(mAnimationPlayer);
     }
 
     private void startAnimation(AnimationDrawable mAnimation) {
@@ -93,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
                 .subscribe(new DisposableCompletableObserver() {
                     @Override
                     public void onComplete() {
-                        updateScore();
+                        updateScore(10);
                         showScore();
                         if (inGame) {
                             updateData(totalDuration);
@@ -107,42 +133,43 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    private void fillAnimationPlayer(AnimationDrawable mAnimation, List<BitmapDrawable> listBitmapPlayer) {
-        for (int i = 0; i < listBitmapPlayer.size(); i++) {
-            mAnimation.addFrame(listBitmapPlayer.get(i), 65);
+    private void fillAnimation(AnimationDrawable mAnimation, List<BitmapDrawable> listBitmap, int duration) {
+        for (int i = 0; i < listBitmap.size(); i++) {
+            mAnimation.addFrame(listBitmap.get(i), duration);
         }
     }
 
-    private List<BitmapDrawable> getListBitmapPlayer() {
+    private List<BitmapDrawable> getListBitmapOrcSlashing() {
         List<BitmapDrawable> frames = new ArrayList<>();
+        List<Bitmap> listBitmap = new ArrayList<>();
 
-        Bitmap bm1;
-        Bitmap bm2;
-        Bitmap bm3;
-        Bitmap bm4;
-        Bitmap bm5;
-        Bitmap bm6;
+        Bitmap bm1 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Slashing_000.png");
+        Bitmap bm2 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Slashing_001.png");
+        Bitmap bm3 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Slashing_002.png");
+        Bitmap bm4 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Slashing_003.png");
+        Bitmap bm5 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Slashing_004.png");
+        Bitmap bm6 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Slashing_005.png");
+        Bitmap bm7 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Slashing_006.png");
+        Bitmap bm8 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Slashing_007.png");
+        Bitmap bm9 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Slashing_008.png");
+        Bitmap bm10 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Slashing_009.png");
+        Bitmap bm11 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Slashing_010.png");
+        Bitmap bm12 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Slashing_011.png");
+        Bitmap bm13 = getBitmapFromAssets(this, "images/orc/slash/0_Orc_Idle_000.png");
 
-        bm1 = getBitmapFromAssets(this, "images/player/attack000.png");
-        bm2 = getBitmapFromAssets(this, "images/player/attack001.png");
-        bm3 = getBitmapFromAssets(this, "images/player/attack002.png");
-        bm4 = getBitmapFromAssets(this, "images/player/attack003.png");
-        bm5 = getBitmapFromAssets(this, "images/player/attack004.png");
-        bm6 = getBitmapFromAssets(this, "images/player/attack000.png");
-
-        BitmapDrawable frame1;
-        BitmapDrawable frame2;
-        BitmapDrawable frame3;
-        BitmapDrawable frame4;
-        BitmapDrawable frame5;
-        BitmapDrawable frame6;
-
-        frame1 = new BitmapDrawable(getResources(), bm1);
-        frame2 = new BitmapDrawable(getResources(), bm2);
-        frame3 = new BitmapDrawable(getResources(), bm3);
-        frame4 = new BitmapDrawable(getResources(), bm4);
-        frame5 = new BitmapDrawable(getResources(), bm5);
-        frame6 = new BitmapDrawable(getResources(), bm6);
+        BitmapDrawable frame1 = new BitmapDrawable(getResources(), bm1);
+        BitmapDrawable frame2 = new BitmapDrawable(getResources(), bm2);
+        BitmapDrawable frame3 = new BitmapDrawable(getResources(), bm3);
+        BitmapDrawable frame4 = new BitmapDrawable(getResources(), bm4);
+        BitmapDrawable frame5 = new BitmapDrawable(getResources(), bm5);
+        BitmapDrawable frame6 = new BitmapDrawable(getResources(), bm6);
+        BitmapDrawable frame7 = new BitmapDrawable(getResources(), bm7);
+        BitmapDrawable frame8 = new BitmapDrawable(getResources(), bm8);
+        BitmapDrawable frame9 = new BitmapDrawable(getResources(), bm9);
+        BitmapDrawable frame10 = new BitmapDrawable(getResources(), bm10);
+        BitmapDrawable frame11 = new BitmapDrawable(getResources(), bm11);
+        BitmapDrawable frame12 = new BitmapDrawable(getResources(), bm12);
+        BitmapDrawable frame13 = new BitmapDrawable(getResources(), bm13);
 
         frames.add(frame1);
         frames.add(frame2);
@@ -150,6 +177,39 @@ public class MainActivity extends AppCompatActivity {
         frames.add(frame4);
         frames.add(frame5);
         frames.add(frame6);
+        frames.add(frame7);
+        frames.add(frame8);
+        frames.add(frame9);
+        frames.add(frame10);
+        frames.add(frame11);
+        frames.add(frame12);
+        frames.add(frame13);
+
+        return frames;
+    }
+
+    private List<BitmapDrawable> getListBitmapGoblinHurt() {
+        List<BitmapDrawable> frames = new ArrayList<>();
+        List<Bitmap> listBitmap = new ArrayList<>();
+
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Idle_000.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Hurt_000.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Hurt_001.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Hurt_002.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Hurt_003.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Hurt_004.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Hurt_005.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Hurt_006.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Hurt_007.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Hurt_008.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Hurt_009.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Hurt_010.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Hurt_011.png"));
+        listBitmap.add(getBitmapFromAssets(this, "images/goblin/hurt/0_Goblin_Idle_000.png"));
+
+        for (int i = 0; i < listBitmap.size(); i++) {
+            frames.add(new BitmapDrawable(getResources(), listBitmap.get(i)));
+        }
 
         return frames;
     }
@@ -178,17 +238,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showScore() {
-        binding.textScore.setText("Score : " + Utils.score);
+        binding.textScore.setText("Gold : " + Utils.score);
     }
 
-    private void updateScore() {
-        Utils.score = Utils.score + 1;
+    private void updateScore(int count) {
+        Utils.score = Utils.score + count;
     }
 
     private void initView() {
-        binding.textScore.setText("Score : " + Utils.score);
-        mAnimation = new AnimationDrawable();
-        mAnimation.setOneShot(false);
-        mAnimation.setVisible(true, true);
+
+        hpEnemy = Utils.getHpEnemy(Utils.currentLevelScene);
+
+        binding.pbHorizontal.setMax(hpEnemy);
+        binding.pbHorizontal.setProgress(hpEnemy);
+        binding.textHpEnemy.setText(String.valueOf(hpEnemy));
+
+        Log.d(TAG, "pbHorizontal.getMax: " + binding.pbHorizontal.getMax());
+        Log.d(TAG, "pbHorizontal.getProgress: " + binding.pbHorizontal.getProgress());
+        Log.d(TAG, "textHpEnemy.getText: " + binding.textHpEnemy.getText());
+
+        binding.textScore.setText("Gold : " + Utils.score);
+        mAnimationPlayer = new AnimationDrawable();
+        mAnimationPlayer.setOneShot(true);
+        mAnimationPlayer.setVisible(true, true);
+
+        mAnimationEnemy = new AnimationDrawable();
+        mAnimationEnemy.setOneShot(true);
+        mAnimationEnemy.setVisible(true, true);
+    }
+
+
+    private void stopAnimationEnemy(AnimationDrawable mAnimation) {
+        if (mAnimation.isRunning()) {
+            mAnimation.stop();
+        }
+    }
+
+    private void startGame() {
+        inGame = true;
+        stopAnimation(mAnimationPlayer);
+        updateData(getTotalDuration(mAnimationPlayer));
+        startAnimation(mAnimationPlayer);
     }
 }
--- app/src/main/java/com/example/testclickergame/Utils.java
+++ app/src/main/java/com/example/testclickergame/Utils.java
@@ -1,5 +1,22 @@
 package com.example.testclickergame;
 
+import java.util.Random;
+
 public class Utils {
     public static int score = 0;
+    public static int maxLevelSceneProgress = 1;
+    public static int currentLevelScene = 2;
+
+    public static int getHpEnemy(int currentLevelScene){
+        int temp = 0;
+
+        int percentScaleCurrentLevelScene = currentLevelScene * 15;
+
+        int iteration1 = 10 * currentLevelScene;
+        int iteration2 = (iteration1 * percentScaleCurrentLevelScene) / 100 + iteration1;
+
+        temp = (iteration2 * (new Random().nextInt(10) + 1)) / 100 + iteration2;
+
+        return temp;
+    }
 }
